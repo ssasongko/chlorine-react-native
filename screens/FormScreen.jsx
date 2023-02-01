c2bc67238c7aa746cb2565fba0945bb3e8948c8a
Nur Sasongko <kokonur1020@gmail.com>
2023-02-01 23:16:26 +0700

    Updated: Form Screen

--- screens/FormScreen.jsx
+++ screens/FormScreen.jsx
@@ -1,52 +1,107 @@
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 import React, { useState } from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Text } from 'react-native-paper';
 import TextField from '../components/TextField';
+import moment from 'moment';
 
 export const FormScreen = () => {
   const [kodeMenu, setKodeMenu] = useState("");
   const [nomorMeja, setnomorMeja] = useState(0);
   const [harga, setHarga] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: currentMode,
+      is24Hour: true,
+    });
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
 
   return (
-    <View>
-      <Text>Form Pesanan</Text>
-      <TextField
-        val={kodeMenu}
-        onChange={setKodeMenu}
-        placeholder="Kode Menu"
-      />
-      <Text>{kodeMenu}</Text>
-      <TextField
-        val={nomorMeja}
-        onChange={setnomorMeja}
-        placeholder="Nomor Meja"
-        inputMode={"numeric"}
-      />
-      <Text>{nomorMeja}</Text>
-      <TextField
-        val={harga}
-        onChange={setHarga}
-        placeholder="Harga"
-        inputMode={"numeric"}
-      />
-      <Text>{harga}</Text>
-      <View>
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', borderColor: 'black', borderBottomWidth: 2 }}>
+        <Text variant="displayMedium">Buat Pesanan</Text>
+      </View>
+
+      <View style={{ marginTop: 20 }}>
+        <TextField
+          val={nomorMeja}
+          onChange={setnomorMeja}
+          placeholder="Nomor Meja"
+          inputMode={"numeric"}
+        />
+        <TextField
+          val={kodeMenu}
+          onChange={setKodeMenu}
+          placeholder="Kode Menu"
+        />
         <TextField
           val={harga}
           onChange={setHarga}
           placeholder="Harga"
           inputMode={"numeric"}
         />
-        <Text>Heaaaaaaaaaaaaaaaallo World</Text>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5 }}>
+          <View style={{ flex: 2 }}>
+            <TextField
+              val={moment(date).format('HH:mm:ss')}
+              placeholder="Time"
+              inputMode={"numeric"}
+              editable={false}
+            />
+          </View>
+          <View style={{ flex: 1, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Button onPress={showTimepicker} title="Choose Time" />
+          </View>
+        </View>
+
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 5 }}>
+          <View style={{ flex: 2 }}>
+            <TextField
+              val={moment(date).format('D MMM YYYY')}
+              placeholder="Date"
+              inputMode={"numeric"}
+              editable={false}
+            />
+          </View>
+          <View style={{ flex: 1, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Button onPress={showDatepicker} title="Choose Date" />
+          </View>
+        </View>
+      </View>
+
+      <View style={{ marginTop: 20 }}>
+        <Text variant="bodyLarge">Nomor Meja: {nomorMeja == 0 ? '-' : nomorMeja}</Text>
+        <Text variant="bodyLarge" style={{ marginTop: 5 }}>Kode Menu: {kodeMenu ? kodeMenu : '-'}</Text>
+        <Text variant="bodyLarge" style={{ marginTop: 5 }}>Harga: {harga == 0 ? '-' : harga}</Text>
+        <Text variant="bodyLarge" style={{ marginTop: 5 }}>Time: {moment(date).format('HH:mm:ss')}</Text>
+        <Text variant="bodyLarge" style={{ marginTop: 5 }}>Date: {moment(date).format('D MMM YYYY')}</Text>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
+  text: {
+    marginTop: 8,
+    fontSize: 30,
+  }
 });
